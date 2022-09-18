3958b3dc7b7408d80d3911b2abd4db3d8476abf7
timtoronto634 <tim225x@gmail.com>
2022-09-18 16:59:24 +0900

    Add func to countup

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,11 +12,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        val countupButton: Button = findViewById(R.id.countup_button)
+        countupButton.setOnClickListener { countupNumber() }
     }
 
     private fun rollDice() {
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countupNumber() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val updated: Int = when (val num = resultText.text.toString().toIntOrNull()) {
+            null -> {
+                1
+            }
+            6 -> {
+                6
+            }
+            else -> {
+                num + 1
+            }
+        }
+        resultText.text = updated.toString()
     }
 }

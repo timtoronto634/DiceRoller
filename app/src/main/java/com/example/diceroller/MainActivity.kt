1d20a0fe77b33e5cdc4f67cc2b0184269b637535
timtoronto634 <tim225x@gmail.com>
2022-09-19 15:49:46 +0900

    Add optimization for old version image.
    Add two image dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,17 +12,20 @@ class MainActivity : AppCompatActivity() {
         lateinit var diceImage : ImageView
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-//        val countupButton: Button = findViewById(R.id.countup_button)
-//        countupButton.setOnClickListener { countupNumber() }
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (randomInt) {
+        val diceImage1: ImageView = findViewById(R.id.dice_image1)
+        diceImage1.setImageResource(getRandomDiceImage())
+        val diceImage2: ImageView = findViewById(R.id.dice_image2)
+        diceImage2.setImageResource(getRandomDiceImage())
+
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -30,22 +33,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
+        return drawableResource
     }
 
-//    private fun countupNumber() {
-//        val resultText: TextView = findViewById(R.id.result_text)
-//        val updated: Int = when (val num = resultText.text.toString().toIntOrNull()) {
-//            null -> {
-//                1
-//            }
-//            6 -> {
-//                6
-//            }
-//            else -> {
-//                num + 1
-//            }
-//        }
-//        resultText.text = updated.toString()
-//    }
 }
